292d400721a904b90340cdd6973a0be164f2c66e
adam147g <adasadas223@wp.pl>
2023-10-11 19:05:55 +0200

    navbar, footer

--- micro projekt/myscript.js
+++ micro projekt/myscript.js
@@ -1,24 +1,34 @@
 document.getElementById("contact-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-  
-    const formData = new FormData(this);
-  
-    fetch("https://formspree.io/adasadas223@wp.pl", {
-      method: "POST",
-      body: formData,
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          // Udało się wysłać e-mail
-          alert("Wiadomość została wysłana. Dziękujemy!");
-          this.reset();
-        } else {
-          // Wystąpił błąd
-          alert("Wystąpił błąd podczas wysyłania wiadomości.");
-        }
-      });
-  });
-  
+  var firstName = document.getElementById("firstName").value.trim();
+  var lastName = document.getElementById("lastName").value.trim();
+  var phoneNumber = document.getElementById("phoneNumber").value.trim();
+
+  // Sprawdzenie, czy imię i nazwisko nie są puste
+  if (firstName === "" || lastName === "") {
+      alert("Proszę wprowadzić zarówno imię, jak i nazwisko.");
+      e.preventDefault();
+      return;
+  }
+
+  // Sprawdzenie, czy imię i nazwisko zaczynają się od dużej litery i nie zawierają cyfr
+  var namePattern = /^[A-ZŁŚ][a-ząćęłńóśźżA-ZŁŚĆĘŃÓŚŹŻ ]+$/;
+  if (!namePattern.test(firstName) || !namePattern.test(lastName)) {
+      alert("Imię i nazwisko muszą zaczynać się od dużej litery i nie mogą zawierać cyfr.");
+      e.preventDefault();
+      return;
+  }
+
+  // Sprawdzenie, czy numer telefonu składa się z dokładnie 9 cyfr
+  var phonePattern = /^\d{9}$/;
+  if (!phonePattern.test(phoneNumber)) {
+      alert("Numer telefonu musi składać się z dokładnie 9 cyfr.");
+      e.preventDefault();
+      return;
+  }
+
+    var message = document.getElementById("message").value;
+    var emailBody = "Imię i Nazwisko: " + fullName + "\nNumer telefonu: " + phoneNumber + "\nWiadomość: " + message;
+    var emailLink = "mailto:adasadas223@wp.pl?subject=Nowa wiadomość&body=" + encodeURIComponent(emailBody);
+    window.location.href = emailLink;
+});
+
