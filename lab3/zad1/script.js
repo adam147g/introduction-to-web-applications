f3fb5467dbe8ab3ae808b7415ff4998a643df9ef
adam147g <adasadas223@wp.pl>
2023-11-08 15:11:47 +0100

    lab3/zad1

--- lab3/zad1/script.js
+++ lab3/zad1/script.js
@@ -1,8 +1,27 @@
 async function logMovies() {
     const response = await fetch("https://dummyjson.com/products");
     const movies = await response.json();
-    console.log(movies);
+    // console.log(movies);
+    const results = [];
     
+    for (let i = 0; i < 30 && i < movies.products.length; i++) {
+      const product = movies.products[i];
+      const productInfo = {
+        nazwa: product.title,
+        opis: product.description,
+        ikonka: product.thumbnail
+      };
+      results.push(productInfo);
+    }
+
+    const listElement = document.getElementById("product-list");
+
+    results.forEach((result, index) => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `<strong>Produkt ${index + 1}:</strong><br>Nazwa: ${result.nazwa}<br>Opis: ${result.opis}<br><img src="${result.ikonka}" alt="Ikonka produktu">`;
+      listElement.appendChild(listItem);
+    });
+
   }
   
 logMovies();
